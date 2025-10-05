864da8bc501bad949dfdc8e1377913f7bc1637f4
51hamu51 <hawks_2023@outlook.jp>
2025-10-05 14:05:56 +0900

    ルームの数字のUI

--- Assets/Sakamoto/Scripts/StageText.cs
+++ Assets/Sakamoto/Scripts/StageText.cs
@@ -4,15 +4,35 @@ using TMPro;
 public class StageTextScript : MonoBehaviour
 {
     [SerializeField] private TextMeshProUGUI stageText;
+    public GameObject stage1;
+    public GameObject stage2;
+    public GameObject stage3;
     void Start()
     {
-
+        if (GameManager.Instance.clearStageNum == 0)
+        {
+            stage2.SetActive(false);
+            stage3.SetActive(false);
+            stage1.SetActive(true);
+        }
+        else if (GameManager.Instance.clearStageNum == 1)
+        {
+            stage1.SetActive(false);
+            stage3.SetActive(false);
+            stage2.SetActive(true);
+        }
+        else if (GameManager.Instance.clearStageNum == 2)
+        {
+            stage1.SetActive(false);
+            stage2.SetActive(false);
+            stage3.SetActive(true);
+        }
     }
 
     void Update()
     {
-        
-            stageText.text = "Stage:" + (GameManager.Instance?.clearStageNum + 1);
-        
+
+        // stageText.text = "Stage:" + (GameManager.Instance?.clearStageNum + 1);
+
     }
 }
