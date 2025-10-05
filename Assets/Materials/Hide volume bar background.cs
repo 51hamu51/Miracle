a27a9d6b5806d972f8c3046256ce2405d365fb1a
具志俐初\makis <vwb08-pc250032@sankogakuen.jp>
2025-10-05 12:30:17 +0900

    画像

---
+++ Assets/Materials/Hide volume bar background.cs
@@ -0,0 +1,23 @@
+
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HeightChange : MonoBehaviour
+{
+    private float heightPos;
+
+    void Start()
+    {
+        heightPos = 0;
+    }
+
+    void Update()
+    {
+        if (transform.localScale.y > 5.0f)
+        {
+            
+        }
+        
+    }
+}
